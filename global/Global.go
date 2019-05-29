d839166ea276a3845683681a62e5dc945deb04da
yuanxs <yuanxs@95szzl.cn>
2019-05-29 13:38:18 +0800

    201905291328

--- global/Global.go
+++ global/Global.go
@@ -7,9 +7,9 @@ import (
 )
 
 const (
-	//PreVersion = "0.0.0 Build20190101"
+	//PreVersion = "0.0.1 Build20190529"
 	//TestVersion = "0.0.0 Build20190101"
-	Version = "0.0.1 Build20190529"
+	Version = "0.0.0 Build20190101"
 )
 
 var Ctx context.Context
